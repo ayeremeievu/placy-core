053fc0cb9424ea32f48030ac5e05d1fbba68544c
ayeremeiev <a.yeremeiev@netconomy.net>
2020-04-25 00:47:42 +0300

    Init commit

---
+++ src/main/java/com/placy/placycore/PlacyCoreApplication.java
@@ -0,0 +1,13 @@
+package com.placy.placycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacyCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacyCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/placy/placycore/PlacyCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placy.placycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacyCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
